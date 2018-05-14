4cc2b5801dfd984aed572c99f7d579081be04195
MrLing <LingHuan5593@qq.com>
2018-05-14 13:13:35 +0800

    180512WebService2_ws_cxf_client  1、使用CXF框架开发调用web service 客户端；2、分析WebService的WSDL文档结构

---
+++ 180512WebService2_ws_cxf_client/src/com/hr/ws_cxf_client/test/ClientTest.java
@@ -0,0 +1,24 @@
+package com.hr.ws_cxf_client.test;
+
+import com.hr.ws.HelloWS;
+import com.hr.ws.HelloWSImplService;
+
+/**
+ *   使用CXF开发调用web service 客户端
+ * @Name  : ClientTest
+ * @Author : LH
+ * @Date : 2018年5月12日 下午7:27:19
+ * @Version : V1.0
+ * 
+ * @Description :
+ */
+public class ClientTest {
+
+			public static void main(String[] args) {
+				HelloWSImplService factory = new HelloWSImplService();
+		        HelloWS helloWS =factory.getHelloWSImplPort();
+		        String result = helloWS.sayHello("BIBli");
+		        System.out.println("cxf_client " +result);
+			}
+
+}
